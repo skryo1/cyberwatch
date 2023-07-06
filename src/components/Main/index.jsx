382d3baf4873592526b9b30ca20a8d9a24fb2ccc
emreciran <otogaming09@gmail.com>
2023-07-06 23:44:02 +0300

    .

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -10,16 +10,16 @@ const Main = () => {
       data-aos-easing="ease-out"
       className="mb-20"
     >
-      <div className="flex items-center justify-between mb-14 max-w-3xl mx-auto max-sm:text-center">
-        <h4 className="text-xl max-sm:text-[18px] text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
+      <div className="flex items-center justify-between mb-14 max-w-3xl mx-auto max-md:text-center ">
+        <h4 className="text-2xl max-sm:text-[18px] text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
           50 CHEATERS CONVICTED
         </h4>
-        <h4 className="text-xl max-sm:text-[18px] text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
+        <h4 className="text-2xl max-sm:text-[18px] text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600">
           INSTALLED IN OVER 50 GAMES
         </h4>
       </div>
 
-      <p className="text-center mb-5 max-w-3xl mx-auto leading-[30px] text-2xl max-sm:text-lg">
+      <p className="text-center mb-5 max-w-3xl mx-auto leading-[30px] text-xl max-sm:text-lg">
         Cyberwatch is an advanced anti-cheat system that combines human
         validation and verification to effectively combat cheating in games.
         With a dedicated team and powerful tools, it swiftly reviews evidence
@@ -29,10 +29,10 @@ const Main = () => {
         all.
       </p>
       <div className="flex items-center justify-center gap-4">
-        <button className="px-6 py-3 bg-gradient-to-br from-green-400 to-green-600 rounded-md hover:bg-gradient-to-br hover:from-green-700 hover:to-green-600  hover:bg-green-600 transition ease-in-out duration-300">
+        <button className="px-6 py-3 bg-gradient-to-br from-green-400 to-green-600 rounded-md  hover:from-green-600 hover:to-green-400  hover:bg-green-600 transition ease-in-out duration-300">
           JOIN DISCORD
         </button>
-        <button className="px-6 py-3 bg-gradient-to-br from-green-400 to-green-600 rounded-md hover:bg-gradient-to-br hover:from-green-700 hover:to-green-600 transition ease-in-out duration-300">
+        <button className="px-6 py-3 bg-gradient-to-br from-green-400 to-green-600 rounded-md  hover:from-green-600 hover:to-green-400 transition ease-in-out duration-300">
           ADD OUR BOT
         </button>
       </div>
